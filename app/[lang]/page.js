af05910e3e189639119206e3bc5acced4355ea67
unknown <talhauslu62@gmail.com>
2025-11-11 22:39:22 +0300

    minor fix

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,5 +1,5 @@
 import { useTranslations } from 'next-intl';
-import Link from 'next/link';
+import { Link } from '@/i18n/navigation';
 import Image from 'next/image';
 import HeroSlider from '@/components/HeroSlider';
 
--- components/HeroSlider.js
+++ components/HeroSlider.js
@@ -1,6 +1,6 @@
 "use client";
 import Image from 'next/image';
-import Link from 'next/link';
+import { Link } from '@/i18n/navigation';
 import { useState, useEffect, useRef } from 'react';
 
 export default function HeroSlider() {
